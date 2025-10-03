0a650364bfcb5e3f8f6fe8919172f624670a2765
Open Learn <eopenlearn@gmail.com>
2025-10-03 16:57:07 +0530

    Update mcq-data.js

--- mcq-data.js
+++ mcq-data.js
@@ -3,35 +3,66 @@ const mcqData = {
     msword: {
         "ප්‍රශ්න පත්‍ර අංක 01": [
             {
-                question: "අක්ෂර ආකෘතිකරණය සඳහා ඔබ තෝරා ගත යුතු TAB ය කුමක්ද?",
-                options: ["Home", "Insert", "Page Layout", "View"],
-                answer: 0,
-                explanation: "අක්ෂර ආකෘතිකරණ සඳහා විකල්පය MS Word හි Home Tab හී පිහිටා ඇත."
+                question: "1. MS Word යනු:",
+                options: ["මෙහෙයුම් පද්ධතියකි.", "වදන් සැකසුම් මෘදුකාංගයකී.", "දත්ත සමුදායක මෘදුකාංගයකි.", "Spreadsheet මෘදුකාංගයකි."],
+                answer: 1,
+                explanation: "MS Word යනු වදන් සැකසුම් මෘදුකාංගයකි."
             },
             {
-                question: "MS Word හි ලේඛනයක් සුරැකීමට යතුරුපුවරු කෙටිමඟ කුමක්ද?",
-                options: [ "Ctrl + P", "Ctrl + S", "Ctrl + O", "Ctrl + N"],
+                question: "2. MS Word 2016/2019 හි default file extension කුමක්ද?",
+                options: [ ".txt", ".xls", ".docx", ".ppt"],
                 answer: 2,
-                explanation: "Ctrl + S යනු MS Word හි ලේඛනයක් සුරැකීමට ඇති යතුරුපුවරු කෙටිමඟයි."
+                explanation: "MS Word හී default file extension .docx වේ."
             },
             {
-                question: "Which option is used to insert a page break?",
-                options: ["Ctrl + Enter", "Ctrl + P", "Ctrl + B", "Alt + Enter"],
-                answer: 0,
-                explanation: "Ctrl + Enter inserts a page break in MS Word."
+                question: "03. Copy කිරීමේ Shortcut key එක කුමක්ද?",
+                options: ["Ctrl + X", "Ctrl + P", "Ctrl + C", "Ctrl + A"],
+                answer: 2,
+                explanation: "Ctrl + C යනු MS Word හි Copy කිරීමට ඇති යතුරුපුවරු කෙටිමගයි."
             },
             {
-                question: "What does Ctrl + B do in MS Word?",
-                options: ["Bold text", "Break page", "Bookmark", "Border"],
-                answer: 0,
-                explanation: "Ctrl + B applies bold formatting to selected text."
+                question: "04. MS Word ගොනුවක් නව නමකින් සුරැකීම සඳහා පහත දෑ අතුරින් කුමක් භාවිතා කරයිද?",
+                options: ["Save", "Save As", "Open", "Close"],
+                answer: 1,
+                explanation: "MS Word ගොනුවක් නව නමකින් සුරැකීම සඳහා Save As විකල්පය භාවිතා කරනු ලැබේ."
             },
             {
-                question: "Which view shows multiple pages at once?",
-                options: ["Print Layout", "Web Layout", "Outline", "Draft"],
+                question: "05. Undo කිරීමේ Shortcut key එක කුමක්ද?",
+                options: ["Ctrl + Y", "Ctrl + U", "Ctrl + Z", "Ctrl + P"],
+                answer: 2,
+                explanation: "Ctrl + Z යනු MS Word හි Undo කිරීමට ඇති යතුරුපුවරු කෙටිමගයි."
+            },
+            {
+                question: "06. Center alignment කිරීමේ Shortcut key එක කුමක්ද?",
+                options: ["Ctrl + L", "Ctrl + E", "Ctrl + R", "Ctrl + J"],
+                answer: 1,
+                explanation: "Ctrl + E යනු MS Word හි Center alignment කිරීමට ඇති යතුරුපුවරු කෙටිමගයි."
+            },
+            {
+                question: "07. MS Word මෘදුකාංගය වැඩි වශයෙන් භාවිතා වන්නේ ?",
+                options: ["පරිගණක ක්‍රීඩා සඳහා", "වීඩියෝ නැරඹීම සඳහා", "ලේඛණ සංස්කරණය සඳහා", "වෙබ් අඩවි සැකසීම සඳහා"],
+                answer: 2,
+                explanation: "MS Word මෘදුකාංගය ලේඛණ සංස්කරණය සඳාහා යොදා ගන්නා මෘදුකාංගයකී."
+            },
+            {
+                question: "08. Text තද පැහැ ගැන්වීම (Bold) කිරීම සඳහා __________ භාවිතා වේ.",
+                options: ["Ctrl + U", "Ctrl + B", "Ctrl + I", "Ctrl + P"],
+                answer: 1,
+                explanation: "Ctrl + B යනු Text තද පැහැ ගැන්වීම සඳහා යොදා ගන්නා යතුරුපුවරු කෙටිමග වේ."
+            },
+            {
+                question: "09. Font group කුමන Tab කාණ්ඩයට අයත් වේද?",
+                options: ["Home", "Insert", "View", "Design"],
                 answer: 0,
-                explanation: "Print Layout view allows you to see multiple pages."
-            }
+                explanation: "MS Word මෘදුකාංගය ලේඛණ සංස්කරණය සඳාහා යොදා ගන්නා මෘදුකාංගයකී."
+            },
+            {
+                question: "10. Document එකක් Print කිරීමට shortcut key එක කුමක්ද?",
+                options: ["Ctrl + S", "Ctrl + O", "Ctrl + F", "Ctrl + P"],
+                answer: 3,
+                explanation: "Ctrl + P යනු MS Word හි Document Print කිරීමට ඇති යතුරුපුවරු කෙටිමගයි."
+            },
+            
         ],
         "ප්‍රශ්න පත්‍ර අංක 02": [
             {
